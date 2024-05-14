dc0d57a688c5263d4f6be3fd549d70832505879f
visler <chrivisl@hotmail.com>
2024-05-14 20:37:17 +0200

    m_m3.py: Construct() model COMPLETE-> now just takes table name to generate all metadata and handles period override

--- sandbox/model/m_m3.py
+++ sandbox/model/m_m3.py
@@ -8,12 +8,15 @@ from pydantic import (
     ValidationError,
     BeforeValidator,
 )
+import datetime
+
 from model_validators import month_, week_, month_dot, week_dot
+from sony import calyear_fiscyear as _conv, dotyear as _dot, calyear_date as _date
 from metadata import SQL_META_DATA as META
 from enums import Period, SnordTable
-from typing import List, Any, Sequence, Annotated
+from typing import Dict, List, Any, Sequence, Annotated, TypeAlias
+period_: TypeAlias = Sequence[month_ | week_ | month_dot | week_dot]
 
-# from sony import calyear_fiscyear as _conv, dotyear as _dot
 
 
 class Init(BaseModel):
@@ -25,43 +28,102 @@ class Init(BaseModel):
     model_config = ConfigDict(use_enum_values=True, validate_default=True)
 
     table: SnordTable | str
-    period: Annotated[List[month_ | week_], BeforeValidator(lambda v: [v] if not isinstance(v, list) else v)] = [202403, 202404, 202405] # Convert user input to list if it isnt alread
-    period_type: Period.Type | None = None
-    period_def: Period.Def | None = None
-    period_sap: Period.Sap | None = None
-    period_tech_name: List[str] | None = None
-    new_cols: List[str] | None = None
+    period: Annotated[List[month_ | week_], BeforeValidator(lambda v: [v] if not isinstance(v, list) else v)] | None = None # Convert user input to list if it isnt already
+    period_type: Period.Type | None = Field(default=None, exclude=True)
+    period_def: Period.Def | None = Field(default=None, exclude=True)
+    period_sap: Period.Sap | None = Field(default=None, exclude=True)
+    period_tech_name: List[str] | None = Field(default=None, exclude=True)
+    new_cols: List[str] | None =  Field(default=None, exclude=True)
+    technical_name: List[str] | None = Field(default=None, exclude=True)
 
 
-    #TODO: Field validator for period before inner validation takes place which should assign period from META dict
+
+    @field_validator('period', mode='before')
+    @classmethod
+    def assign_period(cls, v: Any, info: ValidationInfo) -> List[month_ | week_]:
+        return META[info.data['table']]['period'] if not v else v
+
 
 
     @model_validator(mode='before')
     @classmethod
-    def populate_meta_model(cls, data: Any) -> Any:
+    def populate_meta_model(cls, data: Dict) -> Dict:
         if isinstance(data, dict):
-            # TODO: Currently hard coded for testing
-            data['period_type'] = Period.Type.year_month
-            data['period_def'] = Period.Def.calendar
-            data['period_sap'] = Period.Sap.dot_interval
-            data['period_tech_name'] = ['S_FYPOPT']
-            data['new_cols'] = ['DATE', 'CUSTOMER', 'MATERIAL', 'QUANTITY', 'P3B_1', 'P3B', 'P5', 'MP']
+            data['period_type'] = META[data['table']]['period_type']
+            data['period_def'] = META[data['table']]['period_def']
+            data['period_sap'] = META[data['table']]['period_sap']
+            data['period_tech_name'] = META[data['table']]['period_tech_name']
+            data['new_cols'] = META[data['table']]['new_cols']
             return data
 
 
 
 class Construct(Init):
-    pass
+    period_sap_entry: period_ | None = None
+    period_iso: List[datetime.date] | None = None
 
 
-if __name__ == '__main__':
-    try:
-        print(
-            Init(
-                table='actuals',
-                # period=202010,
-            ).model_dump_json(indent=2)
-        )
-        # print(Init(table='actuals'))
-    except ValidationError as e:
-        print(e.json(indent=2))
+    @field_validator('period_sap_entry')
+    @classmethod
+    def generate_prd_sap_entry(cls, v: Any, info: ValidationInfo) -> period_:
+        period_sap_ = info.data['period_sap']
+        period_sap_entry_final_ = info.data['period']
+
+
+        # Determine period type and definition and convert to fiscal period if necessary
+        if info.data['period_type'] != Period.Type.year_week:
+            if info.data['period_def'] == Period.Def.fiscal:
+                period_sap_entry_final_ = _conv(info.data['period'])
+            else:
+                period_sap_entry_final_ = [str(i) for i in info.data['period']]
+
+
+        if period_sap_ == Period.Sap.interval:
+            if len(period_sap_entry_final_) > 1: # Check whether single input was provided
+                period_sap_entry_final_ = ' - '.join(
+                    [period_sap_entry_final_[0],
+                    period_sap_entry_final_[-1]]
+                )
+            else:
+                period_sap_entry_final_ = period_sap_entry_final_[0]
+
+
+        if period_sap_ == Period.Sap.dot_interval:
+            if len(period_sap_entry_final_) > 1:
+                period_sap_entry_final_ = ' - '.join(
+                    [
+                        _dot(period_sap_entry_final_[0])[0], # _dot() returns a list, hence the [0] to perform .join()
+                        _dot(period_sap_entry_final_[-1])[0]
+                    ]
+                )
+            else:
+                period_sap_entry_final_ = _dot(period_sap_entry_final_[0])[0]
+
+
+        if period_sap_ == Period.Sap.dot_full:
+            period_sap_entry_final_ = _dot(
+                period_sap_entry_final_
+            )
+
+
+        return period_sap_entry_final_
+
+
+
+    @field_validator('period_iso')
+    @classmethod
+    def generate_prd_iso(cls, v: Any, info: ValidationInfo) -> List[datetime.date]:
+        return [datetime.datetime.strptime(d, "%Y-%m-%d").date() for d in _date(info.data['period'])]
+
+
+
+# if __name__ == '__main__':
+#     try:
+#         print(
+#             Construct(
+#                 table='actuals',
+#                 # period=202010,
+#             ).model_dump_json(indent=2)
+#         )
+#     except ValidationError as e:
+#         print(e.json(indent=2))
--- sandbox/model/metadata.py
+++ sandbox/model/metadata.py
@@ -1,61 +1,38 @@
-from typing import List, Sequence, TypedDict
+from typing import List, TypedDict
 import datetime as dt
 from enums import Period
-from model_validators import month_, week_, month_dot, week_dot
+from model_validators import month_, week_
 from pydantic import (
     BaseModel,
-    field_validator,
-    Field,
     ConfigDict,)
-# from sony import sony_calendar as sc
+from sony import sony_calendar as sc
 
 
-# class BasePeriod(BaseModel):
-#     model_config = ConfigDict(validate_default=True) # Ensure validation of default values, thus guaranteeing output
-#
-#     month: List[month_] = Field(
-#         kw_only=True,
-#         max_length=12, # Max list length
-#         examples=[202403, 202404],
-#         default_factory=list,)
-#
-#
-#     week: List[week_] = Field(
-#         kw_only=True,
-#         max_length=3, # Max list length
-#         examples=[202333, 202334],
-#         default_factory=list,)
-#
-#     last_3_months: List[month_] = sc().get('last_3_months')
-#     last_2_months: List[month_] = sc().get('last_2_months')
-#     last_3_weeks: List[week_] = sc().get('last_3_weeks')
-#     next_4_months: List[month_] = sc().get('nx4m_incl')
-#     next_6_months: List[month_] = sc(
-#                                 year=dt.datetime.now().year,
-#                                 month=dt.datetime.now().month,
-#                                 ranger=5).get('ranger')
-#     rest_fy: List[month_] = sc().get('rest_of_fy')
-#
-#
-#     @field_validator('month', 'week', mode='before')
-#     @classmethod
-#     def check_list(cls, v):
-#         if not isinstance(v, list):
-#             return [v]
-#         else:
-#             return v
+class DefaultPeriod(BaseModel):
+    model_config = ConfigDict(validate_default=True) # Ensure validation of default values, thus guaranteeing output
+
+    last_3_months: List[month_] = sc().get('last_3_months')
+    last_2_months: List[month_] = sc().get('last_2_months')
+    last_3_weeks: List[week_] = sc().get('last_3_weeks')
+    next_4_months: List[month_] = sc().get('nx4m_incl')
+    next_6_months: List[month_] = sc(
+                                year=dt.datetime.now().year,
+                                month=dt.datetime.now().month,
+                                ranger=5).get('ranger')
+    rest_fy: List[month_] = sc().get('rest_of_fy')
+
 
 
 class SQLTableContent(TypedDict):
     alias: List[str]
     period: List[month_ | week_]
     # TODO: Study up on invariant vs covariant, i.e. using Sequence over List
-    # period_sap_entry: Sequence[month_ | week_ | month_dot | week_dot]
     period_type: Period.Type
     period_def: Period.Def
     period_sap: Period.Sap
     period_tech_name: List[str]
     new_cols: List[str]
+    technical_name: List[str]
 
 
 class SQLMetaData(TypedDict):
@@ -66,22 +43,22 @@ class SQLMetaData(TypedDict):
 SQL_META_DATA: SQLMetaData = {
     'actuals': {
         'alias': [ 'DS_1' ],
-        'period': ['202403', '202404', '202405'],#BasePeriod().last_3_months,
-        # 'period_sap_entry': # TODO: To be calculated inside Construct class
+        'period': DefaultPeriod().last_3_months,
         'period_type': Period.Type.year_month,
         'period_def': Period.Def.calendar,
-        'period_sap': Period.Sap.dot_interval,
+        'period_sap': Period.Sap.interval,
         'period_tech_name': ['S_FYPOPT'],
         'new_cols': ['DATE', 'CUSTOMER', 'MATERIAL', 'QUANTITY', 'P3B_1', 'P3B', 'P5', 'MP'],
+        'technical_name': ['YYEU_C_SEMFBMP_CI_SUMMARY'],
     },
     'sst': {
         'alias': [ 'DS_3' ],
-        'period': ['202416', '202417', '202418'],#BasePeriod().last_3_weeks,
-        # 'period_sap_entry': #BasePeriod().last_3_weeks,
+        'period': DefaultPeriod().last_3_weeks,
         'period_type': Period.Type.year_week,
         'period_def': Period.Def.calendar,
         'period_sap': Period.Sap.full,
         'period_tech_name': ['S_CALWK_MS'],
         'new_cols': ['DATE', 'CUSTOMER', 'EXT_ID', 'MATERIAL', 'SELLOUT', 'STOCK'],
+        'technical_name': ['SSE_CPFRSTSAM_STORHARM'],
     },
 }
